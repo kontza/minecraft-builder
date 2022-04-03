2a7be701bfea280b7a6088e89f35f0d0571445af
Rumpu-Jussi <kontza@gmail.com>
2022-04-03 14:46:59 +0300

    Downloader function added.

--- paper_loader/paper_loader.go
+++ paper_loader/paper_loader.go
@@ -3,8 +3,14 @@ package paper_loader
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/gocolly/colly"
 )
 
@@ -40,13 +46,77 @@ type PaperLoader interface {
 }
 
 type paperLoader struct {
+	url       *url.URL
 	collector *colly.Collector
+}
+
+// WriteCounter counts the number of bytes written to it. It implements to the io.Writer interface
+// and we can pass this into io.TeeReader() which will report progress on each write cycle.
+type WriteCounter struct {
+	Total uint64
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+
+func (wc WriteCounter) PrintProgress() {
+	// Clear the line by using a character return to go back to the start and remove
+	// the remaining characters by filling it with spaces
+	log.Printf("\r%s", strings.Repeat(" ", 35))
+
+	// Return again and print current status of download
+	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
+	log.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory. We pass an io.TeeReader
+// into Copy() to report progress on the download.
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file, but give it a tmp file extension, this means we won't overwrite a
+	// file until it's downloaded, but we'll remove the tmp extension once downloaded.
+	out, err := os.Create(filepath + ".tmp")
+	if err != nil {
+		return err
+	}
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		out.Close()
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create our progress reporter and pass it to be used alongside our writer
+	counter := &WriteCounter{}
+	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
+		return err
+	}
+
+	// The progress use the same line so print a new line once it's finished downloading
+	log.Println()
+
+	// Close the file without defer so it can happen before Rename()
+	out.Close()
+
+	if err = os.Rename(filepath+".tmp", filepath); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (l paperLoader) download(project string, version string, build int, artifact string) {
 	log.Println("Artifact to load", artifact)
 	// https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/277/downloads/paper-1.18.2-277.jar
-	log.Println(fmt.Sprintf("https://papermc.io/api/v2/projects/%s/versions/%s/builds/%d/downloads/%s", project, version, build, artifact))
+	sourceUrl := fmt.Sprintf("%s/%s/versions/%s/builds/%d/downloads/%s", l.url.String(), project, version, build, artifact)
+	DownloadFile(artifact, sourceUrl)
 }
 
 func (l paperLoader) loadBuild(project string, version string, buildNumber int) {
@@ -61,7 +131,7 @@ func (l paperLoader) loadBuild(project string, version string, buildNumber int) 
 		}
 	})
 	// https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/277
-	c.Visit(fmt.Sprintf("https://papermc.io/api/v2/projects/%s/versions/%s/builds/%d", project, version, buildNumber))
+	c.Visit(fmt.Sprintf("%s/%s/versions/%s/builds/%d", l.url.String(), project, version, buildNumber))
 }
 
 func (l paperLoader) loadVersion(project string, version string) {
@@ -76,7 +146,7 @@ func (l paperLoader) loadVersion(project string, version string) {
 		}
 	})
 	// https://papermc.io/api/v2/projects/paper/versions/1.18.2/
-	c.Visit(fmt.Sprintf("https://papermc.io/api/v2/projects/%s/versions/%s", project, version))
+	c.Visit(fmt.Sprintf("%s/%s/versions/%s", l.url.String(), project, version))
 }
 
 func (l paperLoader) LoadProject(project string) {
@@ -90,7 +160,7 @@ func (l paperLoader) LoadProject(project string) {
 			l.loadVersion(project, versions.Versions[len(versions.Versions)-1])
 		}
 	})
-	c.Visit(fmt.Sprintf("https://papermc.io/api/v2/projects/%s", project))
+	c.Visit(fmt.Sprintf("%s/%s", l.url.String(), project))
 }
 
 func (l paperLoader) LoadLatest(f ProjectsCallback) {
@@ -104,12 +174,18 @@ func (l paperLoader) LoadLatest(f ProjectsCallback) {
 			f(&projects.Projects)
 		}
 	})
-	c.Visit("https://papermc.io/api/v2/projects")
+	c.Visit(l.url.String())
 }
 
 func NewPaperLoader() PaperLoader {
 	loader := &paperLoader{
 		collector: colly.NewCollector(),
+		url:       &url.URL{},
+	}
+	if parsedUrl, err := url.Parse("https://papermc.io/api/v2/projects"); err != nil {
+		log.Println("Failed to parse URL", err)
+	} else {
+		loader.url = parsedUrl
 	}
 	loader.collector.OnError(func(r *colly.Response, err error) {
 		log.Println("Failed to find the latest:", err)
